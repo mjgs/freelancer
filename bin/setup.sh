16a1d8292cf4b611773d669d7ebe8b36a277a4f4
Mark Smith <markjgsmith@gmail.com>
2018-12-09 23:13:54 +0700

    Update static site paths

--- bin/setup.sh
+++ bin/setup.sh
@@ -12,8 +12,8 @@ cp ./lib/data/pricing.js.sample ./lib/data/pricing.js
 echo "creating file: ./lib/data/profile.js"
 cp ./lib/data/profile.js.sample ./lib/data/profile.js
 
-echo "creating file: ./lib/static/homepage/index.html"
-cp ./lib/static/homepage/index.html.sample ./lib/static/homepage/index.html
+echo "creating file: ./static/homepage/index.html"
+cp ./static/homepage/index.html.sample ./static/homepage/index.html
 
 echo "Follow the README setup instructions to get the website running"
 
